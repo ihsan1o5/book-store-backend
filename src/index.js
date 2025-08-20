329f8babded2bae83100716ef2e3faa5c6e7ad9b
Ihsan Ullah <Ihsan Ullah@dha360.local>
2025-08-20 14:51:14 +0500

    incresed upload limit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 job.start(); // Start the cron job
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
